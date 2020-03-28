c3217f30ec68b25d49a1385410a9f7a53d911b81
Evan McKinley <elmckinley03@gmail.com>
2020-03-28 13:30:20 -0600

    after lab 8 , it fails with H2 errors, Mar 28 1:30

--- MySkiLift/src/edu/osu/cse5234/controller/PaymentInfo.java
+++ MySkiLift/src/edu/osu/cse5234/controller/PaymentInfo.java
@@ -1,19 +1,12 @@
 package edu.osu.cse5234.controller;
 //this is the PaymentInfo model class
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import edu.osu.cse5234.model.LineItem;
 
 @Entity
 @Table(name="PAYMENT_INFO")
--- MySkiLift/src/edu/osu/cse5234/controller/ShippingInfo.java
+++ MySkiLift/src/edu/osu/cse5234/controller/ShippingInfo.java
@@ -1,14 +1,10 @@
 package edu.osu.cse5234.controller;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
